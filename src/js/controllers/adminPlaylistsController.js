b23d9c430ece01b57b845862a99c627054c37ac8
Denis_TKM <denis.tuekam@gmail.com>
2017-06-15 02:39:24 +0200

    saves changes

--- src/js/controllers/adminPlaylistsController.js
+++ src/js/controllers/adminPlaylistsController.js
@@ -3,4 +3,8 @@
 app.controller('PlaylistsController', function($scope, $http, $filter, editableOptions, editableThemes, Playlists) {
   $scope.playlists = Playlists.show_all(); //fetch all Playlists. Issues a GET to /api/Playlists
   console.log($scope.playlists);
+
+	  $scope.dateCreation=function(date) {
+	 	return new Date(date).toString();
+	};
 });
