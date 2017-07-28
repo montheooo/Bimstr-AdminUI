83b24fca88ae2f067145d73361b8bfbef67f48f6
montheooo <montheooo@yahoo.fr>
2017-07-28 16:19:30 +0200

    remove hash tag in url  Changes to be committed: 	modified:   src/js/config.router.js
     Untracked files:
    	ngFacebook-master.zip
    	npm-debug.log
    	src/tpl/album.html

--- src/js/config.router.js
+++ src/js/config.router.js
@@ -17,7 +17,18 @@
   [          '$stateProvider', '$urlRouterProvider', 'JQ_CONFIG', 'MODULE_CONFIG', '$authProvider', '$ocLazyLoadProvider','$locationProvider',
   function ($stateProvider,   $urlRouterProvider, JQ_CONFIG, MODULE_CONFIG, $authProvider, $ocLazyLoadProvider, $locationProvider) {
     
-  
+     //check browser support
+        if(window.history && window.history.pushState){
+      //  $locationProvider.html5Mode(true); //will cause an error $location in HTML5 mode requires a  tag to be present! Unless you set baseUrl tag after head tag like so: <head> <base href="/">
+
+         // to know more about setting base URL visit: https://docs.angularjs.org/error/$location/nobase
+
+         // if you don't wish to set base URL then use this
+         $locationProvider.html5Mode({
+                 enabled: true,
+                 requireBase: false
+          }); 
+        }
 
     var layout = "tpl/app.html";
     if(window.location.href.indexOf("material") > 0){
